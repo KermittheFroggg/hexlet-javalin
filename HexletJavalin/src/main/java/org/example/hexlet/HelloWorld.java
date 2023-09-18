1bde2ee5b9ae72230321127eaec2f7f2d5fdcc8c
KermittheFroggg <santamantana@gmail.com>
2023-09-18 14:17:46 +0300

    paramPath

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,5 +1,7 @@
 package org.example.hexlet;
 
+import io.javalin.http.NotFoundResponse;
+
 import io.javalin.Javalin;
 
 public class HelloWorld {
@@ -10,12 +12,26 @@ public class HelloWorld {
         });
         // Описываем что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
-        app.get("/users", ctx -> ctx.result("GET /users"));
-        app.post("/users", ctx -> ctx.result("POST /users"));
         app.get("/hello", ctx -> {
             String name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
-            ctx.result("Hello, " + name  + "!");
+            ctx.result("Hello, " + name + "!");
         });
+        app.get("/courses/{id}", ctx -> {
+            ctx.result("Course ID: " + ctx.pathParam("id"));
+        });
+
+        app.get("/courses/{courseId}/lessons/{id}", ctx -> {
+            ctx.result("Course ID: " + ctx.pathParam("courseId"));
+            ctx.result("Lesson ID: " + ctx.pathParam("id"));
+        });
+        app.get("/users/{id}/", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
+        });
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
+            ctx.result("Post ID: " + ctx.pathParam("postId"));
+        });
+
         app.start(7070); // Стартуем веб-сервер
     }
 }
