c2c2306ac2004055e7f0e12771724a5889ecb7a5
KermittheFroggg <santamantana@gmail.com>
2023-09-19 13:30:59 +0300

    template

--- HexletJavalin/jte-classes/gg/jte/generated/ondemand/courses/JteshowGenerated.java
+++ HexletJavalin/jte-classes/gg/jte/generated/ondemand/courses/JteshowGenerated.java
@@ -2,16 +2,20 @@ package gg.jte.generated.ondemand.courses;
 import org.example.hexlet.dto.courses.CoursePage;
 public final class JteshowGenerated {
 	public static final String JTE_NAME = "courses/show.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,2,2,2,6,6,9,9,9,10,10,10,13};
+	public static final int[] JTE_LINE_INFO = {0,0,2,2,2,4,4,6,6,7,7,7,8,8,8,9,9,9};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, CoursePage page) {
-		jteOutput.writeContent("\n<html lang=\"en\">\n\n");
-		jteOutput.writeContent("\n<body>\n<main>\n    <h1>");
-		jteOutput.setContext("h1", null);
-		jteOutput.writeUserContent(page.getCourse().getName());
-		jteOutput.writeContent("</h1>\n    <p>");
-		jteOutput.setContext("p", null);
-		jteOutput.writeUserContent(page.getCourse().getDescription());
-		jteOutput.writeContent("</p>\n</main>\n</body>\n</html>");
+		jteOutput.writeContent("\n");
+		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\n    <h1>");
+				jteOutput.setContext("h1", null);
+				jteOutput.writeUserContent(page.getCourse().getName());
+				jteOutput.writeContent("</h1>\n    <p>");
+				jteOutput.setContext("p", null);
+				jteOutput.writeUserContent(page.getCourse().getDescription());
+				jteOutput.writeContent("</p>\n");
+			}
+		});
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		CoursePage page = (CoursePage)params.get("page");
